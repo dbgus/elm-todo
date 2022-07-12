3f19a89a5eaa444eaa8b7cd976bbbc52a36c1e6f
yuhyeon <yuhyeon.e@ziptoss.com>
2022-07-12 10:49:01 +0900

    feat: todo item status

--- src/Main.elm
+++ src/Main.elm
@@ -4,7 +4,6 @@ import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
-import List as L
 import Todo exposing (Todo)
 
 
@@ -24,7 +23,7 @@ type alias Model =
 
 init : Model
 init =
-    { todo = [ Todo "출근하기" True, Todo "퇴근하기" False ]
+    { todo = []
     , newTodo = ""
     }
 
@@ -32,6 +31,7 @@ init =
 type Event
     = TodoUpdate String
     | Submit
+    | Complated Int
 
 
 update : Event -> Model -> Model
@@ -42,6 +42,17 @@ update event model =
 
         Submit ->
             { model | todo = Todo model.newTodo False :: model.todo }
+
+        Complated compltedItemIndex ->
+            let
+                complatUpdate index item =
+                    if index == compltedItemIndex then
+                        { item | check = not item.check }
+
+                    else
+                        item
+            in
+            { model | todo = List.indexedMap complatUpdate model.todo }
 
 
 view : Model -> Html Event
@@ -60,14 +71,15 @@ view model =
                 , span []
                     [ text "status" ]
                 ]
-            , div [] (L.indexedMap viewTodoItem model.todo)
+            , div [] (List.indexedMap viewTodoItem model.todo)
             ]
         ]
 
 
+
 viewTodoItem : Int -> Todo -> Html Event
 viewTodoItem index todo =
-    div [ style "margin" "15px auto", style "display" "grid", style "grid-template-columns" "85% 15%" ]
+    div [ style "margin" "15px auto", style "display" "grid", style "grid-template-columns" "85% 10% 5%" ]
         [ span []
             [ text todo.content ]
         , span []
@@ -79,4 +91,5 @@ viewTodoItem index todo =
                     "미완료"
                 )
             ]
+        , input [ type_ "checkBox", onClick (Complated index) ] [ text "상태 변경" ]
         ]
